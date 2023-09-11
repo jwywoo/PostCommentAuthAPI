23721a9bed342068ca1947b7e2aef438e947abbd
jwywoo <jwywoo26@gmail.com>
2023-09-11 14:10:44 +0900

    Second Commit: Comment and Board Updated

--- src/main/java/com/example/postcommentauthapi/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/postcommentauthapi/comment/dto/CommentRequestDto.java
@@ -1,4 +1,9 @@
 package com.example.postcommentauthapi.comment.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentRequestDto {
+    private Long boardId;
+    private String content;
 }
--- src/main/java/com/example/postcommentauthapi/comment/dto/CommentResponseDto.java
+++ src/main/java/com/example/postcommentauthapi/comment/dto/CommentResponseDto.java
@@ -1,4 +1,23 @@
 package com.example.postcommentauthapi.comment.dto;
 
+import com.example.postcommentauthapi.comment.entity.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class CommentResponseDto {
+    private final Long id;
+    private final String content;
+    private final String username;
+    private final LocalDateTime createAt;
+    private final LocalDateTime modifiedAt;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.createAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+        this.username = comment.getUsername();
+    }
 }
